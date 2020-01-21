741a5c291c7ba4c8ab2b28c0fb35ce524bd82816
kostadin <isheo@ru-se.com>
2020-01-21 11:10:30 +0000

    Implement accept answer as best answer

--- app/Answer.php
+++ app/Answer.php
@@ -39,4 +39,8 @@ class Answer extends Model
             $answer->question->decrement('answers_count');
         });
     }
+
+    public function getIsBestAttribute(){
+        return $this->id == $this->question->best_answer_id ? 'vote-accepted' : '';
+    } 
 }
